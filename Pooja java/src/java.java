1419a7bda8ed3bffe0b8fea6e87334ac429d1b3a
velisalapoojitha-maker <velisalapoojitha@gmail.com>
2026-05-19 06:13:41 +0000

    All done

---
+++ Pooja java/src/java.java
@@ -0,0 +1,10 @@
+
+public class java {
+		public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		System.out.println("Welcome to Java programming.");
+		System.out.println("This is a simple Java program.");
+		System.out.println("Java is a versatile programming language.");
+	}
+
+}
